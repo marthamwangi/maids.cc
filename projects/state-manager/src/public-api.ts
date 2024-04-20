94183fc3839c89b7b480b448e0438bb12f674550
marthamwangi <martyturing@gmail.com>
2024-04-20 23:31:20 +0300

    chore: reefactor unused files

--- projects/state-manager/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of state-manager
- */
-
-export * from './lib/state-manager.service';
-export * from './lib/state-manager.component';
-export * from './lib/state-manager.module';
